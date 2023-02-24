14607a88859f75f953212c900323cca95c211334
Sheila <shillahchep96@gmail.com>
2023-02-24 09:02:16 -0800

    print a new line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -4,8 +4,10 @@
  * print_line - draws a straight line in the terminal
  * @n: number of times the character _ should be printed
  */
-void print_line(int n)
+void print_line(int)
 {
+	int n;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
